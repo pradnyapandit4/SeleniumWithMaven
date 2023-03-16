aa3577b028b16854f0efc38ca4877453ec8a3fb0
Pradnya <pradnyapandit4@gmail.com>
2023-03-16 23:54:23 +0530

    added search customer file and modified edit customer file

---
+++ src/test/java/learn/SeleniumwithMaven/Tc_customer2.java
@@ -0,0 +1,11 @@
+package learn.SeleniumwithMaven;
+
+public class Tc_customer2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//This is my search customer test case...
+
+	}
+
+}
--- src/test/java/learn/SeleniumwithMaven/tc_customer.java
+++ src/test/java/learn/SeleniumwithMaven/tc_customer.java
@@ -1,2 +1,10 @@
 //This is a new test cases
 //This test will check  editing for customer
+
+//This is code added newly
+
+//This is code added newly
+//This is code added newly
+//This is code added newly
+
+//This is code added newly
